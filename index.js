44fd66f621c4b15e41f431fbf18f49e42e09f9d3
loclv <masamonedante@gmail.com>
2020-07-17 13:55:00 +0900

    refactoring backend: socket handler

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const http = require('http');
 
 const socketServer = http.createServer(app);
-const io = require('socket.io')(socketServer);
+const handleIo = require('./handleIo');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -15,28 +15,7 @@ app.get('/', (req, res) => res.render('home'));
 
 createPeerServer(app, http);
 
-// socket
-
-const userArr = [];
-
-io.on('connection', socket => {
-    socket.on('USER_REGISTER', user => {
-        const isExist = userArr.some(e => e.name === user.name);
-
-        socket.peerId = user.peerId;
-
-        if (isExist) return socket.emit('USER_NAME_EXISTS');
-
-        userArr.push(user);
-        socket.emit('USER_LIST_UPDATE', userArr);
-        socket.broadcast.emit('NEW_USER_ONLINE', user);
-    });
-
-    socket.on('disconnect', () => {
-        const index = userArr.findIndex(user => user.peerId === socket.peerId);
-        userArr.splice(index, 1);
-        io.emit('USER_DISCONNECT', socket.peerId);
-    });
-});
+// socket.io
+handleIo(socketServer);
 
 socketServer.listen(process.env.PORT || 3000, () => console.log('started!'));
